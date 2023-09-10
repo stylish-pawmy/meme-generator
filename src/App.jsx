fc0426465473e075fc7cd73df1c873654a2db75b
stylish-pawmy <ambesi2026@gmail.com>
2023-09-10 18:22:26 +0100

    Add and Style input form

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { default as NavBar } from "./components/NavBar.jsx";
+import { default as InputForm } from "./components/InputForm.jsx";
 
 // Styling
 import './App.css'
@@ -8,6 +9,9 @@ function App() {
   return (
     <>
       <NavBar />
+      <main>
+        <InputForm />
+      </main>
     </>
   )
 }
