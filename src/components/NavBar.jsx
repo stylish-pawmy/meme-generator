2d2ec5d5a4a15cf5fd240a4b8d0b35657dfd5828
stylish-pawmy <ambesi2026@gmail.com>
2023-09-10 17:46:50 +0100

    Style NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,12 @@
 import "../App.css";
+import { default as FoolHatImg } from "../assets/fool-hat.svg";
 
 export default function NavBar()
 {
     return (
         <header>
             <nav>
-                <img />
+                <img src={FoolHatImg} />
                 <h3>Meme Generator</h3>
             </nav>
         </header>
